a06cebfd7713b11773e3a4d88d65c930d56a4e97
FairyTailAdmin <fairytailadmin@mailinator.com>
2013-02-05 10:45:01 +0800

    Added FairyJill updates.

--- src/com/tail/util/DateUtil.java
+++ src/com/tail/util/DateUtil.java
@@ -9,4 +9,7 @@ public class DateUtil
 	public static boolean isBlank(String date){
 		return date == null || date.trim().length() <= 0;
 	}
+	public static boolean isNotBlank(String date){
+		return !isBlank(date);
+	}
 }
