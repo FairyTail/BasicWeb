fc84d443c0ec0627d15cfd0083c1f4f4fee08d2f
FairyTailAdmin <fairytailadmin@mailinator.com>
2013-02-10 20:02:48 +0800

    added isNotBlank method to StringUtil class.

--- src/com/basicweb/util/StringUtil.java
+++ src/com/basicweb/util/StringUtil.java
@@ -5,4 +5,8 @@ public class StringUtil
 	public static boolean isBlank(String s){
 		return s == null || s.trim().length() <= 0;
 	}
+
+	public static boolean isNotBlank(String s){
+		return !isBlank(s);
+	}
 }
